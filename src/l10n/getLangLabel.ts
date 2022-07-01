3ab886edbff2a2b8813b77194e9c579de79f803a
Poppe <djpo@users.noreply.github.com>
2022-07-01 23:49:59 +0200

    display parentheses around unknown lang code

--- src/l10n/getLangLabel.ts
+++ src/l10n/getLangLabel.ts
@@ -9,7 +9,7 @@ export function getLangLabel(langCode: Language) {
     if (langLabels[langCode][interfaceLang]) {
       return langLabels[langCode][interfaceLang];
     }
-    return langCode;
+    return `(${langCode})`;
   }
-  return langCode;
+  return `(${langCode})`;
 }
