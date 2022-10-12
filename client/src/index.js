a7606952599368ff9a39022cfc1422c595c7ef0d
Poppe <djpo@users.noreply.github.com>
2022-10-12 16:38:56 +0200

    upgrade to React 18; fix root element render

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
