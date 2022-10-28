9766b16f17321e5228f03632357ab514255b3444
Poppe <djpo@users.noreply.github.com>
2022-10-28 14:40:52 +0200

    add redux actions for fetching saved cycles; add test button

--- client/src/pages/saved/SavedCyclesPage.tsx
+++ client/src/pages/saved/SavedCyclesPage.tsx
@@ -1,15 +1,38 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
-import { selectSavedCycles } from "../../redux/selectors";
+import { selectSavedCycles, selectIsFetchingSavedCycles } from "../../redux/selectors";
+import {
+  // fetchSavedCycles,
+  updateIsFetchingSavedCycles,
+} from "../../redux/actions";
 import type { SavedCycle, SavedCycleStep } from "../../types";
 import "./SavedCyclePage.css";
 
 const SavedCyclesPage = (): JSX.Element => {
   const savedCycles: SavedCycle[] = useSelector(selectSavedCycles);
+  const isFetchingSavedCycles: boolean = useSelector(selectIsFetchingSavedCycles);
+
+  const dispatch = useDispatch();
+
+  const handleGetSavedCycles = (): void => {
+    // dispatch(fetchSavedCycles());
+    dispatch(updateIsFetchingSavedCycles(!isFetchingSavedCycles));
+  };
+
+  const buttonClassName = isFetchingSavedCycles ? "test-button" : "";
 
   return (
     <>
       <Link to="/">HomePage</Link>
+      <p />
+
+      <button
+        // disabled={isFetchingSavedCycles}
+        className={`primary-button ${buttonClassName}`}
+        onClick={() => handleGetSavedCycles()}
+      >
+        {isFetchingSavedCycles ? "getting..." : "get saved cycles"}
+      </button>
       <p />
 
       {savedCycles ? (
