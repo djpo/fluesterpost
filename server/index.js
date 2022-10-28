5b8212298df175c7ebe9de618a31f7d982020c02
Poppe <djpo@users.noreply.github.com>
2022-10-28 17:15:13 +0200

    server fetches saved cycles from mongo db (#17)

--- server/index.js
+++ server/index.js
@@ -1,21 +1,75 @@
 const path = require("path");
 const express = require("express");
+var { graphqlHTTP } = require("express-graphql");
+var { buildSchema } = require("graphql");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 const PORT = process.env.PORT || 3003;
+
+// create schema
+const schema = buildSchema(`
+  type CycleStep {
+    lang: String!
+    text: String!
+  }
+  type Cycle {
+    _id: ID!
+    originLang: String!
+    originText: String!
+    steps: [CycleStep!]!
+  }
+  type Query {
+    getCycles(last: Int): [Cycle!]!
+  }
+`);
+
+// query resolver functions
+const root = {
+  getCycles: ({ last }) => {
+    const dbUrl = process.env.MONGO_URL;
+    const user = process.env.MONGO_USER;
+    const password = process.env.MONGO_PASSWORD;
+    const connectionOptions = "?retryWrites=true&w=majority";
+    const uri = `mongodb+srv://${user}:${password}@${dbUrl}/${connectionOptions}`;
+    const client = new MongoClient(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverApi: ServerApiVersion.v1,
+    });
+    async function run() {
+      try {
+        await client.connect();
+
+        const collection = await client.db(process.env.DB_NAME).collection("cycles");
+
+        // TODO: limit results to int `last`
+        const myCycles = await collection.find().toArray();
+        return myCycles;
+      } finally {
+        await client.close();
+      }
+    }
+    return run().catch(console.dir);
+  },
+};
 
 const app = express();
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
 
-// Handle GET requests to /api route
-app.get("/api", (req, res) => {
-  setTimeout(() => {
-    res.json({
-      message: `${Math.floor(Math.random() * 100)}`,
-    });
-  }, 800);
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+  })
+);
 
 // All other GET requests not handled before will return our React app
 app.get("*", (req, res) => {
