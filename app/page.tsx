09cb6e4a90dc47b1a791211293be3919b1da41d5
Poppe <djpo@users.noreply.github.com>
2025-06-10 13:49:35 +0200

    stop fetching supported languages on Home

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,11 @@
 import { Instructions } from "@/ui/instructions/instructions";
 import { Steps } from "@/ui/steps/steps";
 import { defaultErrorMessage } from "@/default-values";
-import { fetchLanguages } from "@/lib/fetchLanguages";
 import styles from "@/ui/home.module.css";
 import { ErrorMessage } from "@/types";
 
 export default async function Home(): Promise<React.JSX.Element> {
   const errorMessage: ErrorMessage = defaultErrorMessage;
-
-  const supportedLangs = await fetchLanguages();
-  console.log("Page > supportedLangs:");
-  console.log(supportedLangs);
 
   return (
     <div className={styles.appBody}>
