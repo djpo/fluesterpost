df40e992de892067be5765ec0121a09e98c1981e
Poppe <djpo@users.noreply.github.com>
2025-09-09 16:28:19 +0200

    remove Saved page header

--- app/saved/page.tsx
+++ app/saved/page.tsx
@@ -10,8 +10,6 @@ export default async function Saved(): Promise<React.JSX.Element> {
   return (
     <div className="p-3 flex justify-center">
       <div className="w-full max-w-150">
-        <h1 className="text-lg mb-5">Users&apos; saved cycles</h1>
-
         {cycles.map((cycle: Cycle, cycleIndex: number) => {
           return (
             <SavedCycle
