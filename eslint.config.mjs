ec78bc3e31f3c101fd619f9113624bb438ba9c6a
Poppe <djpo@users.noreply.github.com>
2025-12-08 17:08:33 +0100

    upgrade next to 16.0.8 (npx @next/codemod@canary upgrade latest)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,17 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ["next/core-web-vitals", "next/typescript"],
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off",
-    },
-  }),
-];
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, ...compat.config({
+  rules: {
+    "@typescript-eslint/no-unused-vars": "off",
+  }
+}), {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
